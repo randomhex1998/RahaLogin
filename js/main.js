91ca81df3cd910b45886b6f8e47e76c2f9f9e42d
Rayan <randomhex1998@gmail.com>
2022-08-25 19:30:59 +0430

    clean css codes

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@ let modeIco = document.querySelector(".mode-area i");
 let preLoader = document.querySelector(".pre-loader");
 
 mode.addEventListener("click", function () {
+    mode.classList.add("anime");
+
+    mode.addEventListener("animationend", function () {
+        mode.classList.remove("anime");
+    });
+
     if (modeIco.className.includes("fa-sun")) {
         modeIco.classList.remove("fa-sun");
         modeIco.classList.toggle("fa-moon");
         modeIco.style.color = "#fff";
-        document.body.classList.add("whitetexts");
+        document.body.classList.add("dark-mode");
         localStorage.setItem("theme", "dark");
     } else {
         modeIco.classList.add("fa-sun");
         modeIco.classList.remove("fa-moon");
-        document.body.classList.remove("whitetexts");
+        document.body.classList.remove("dark-mode");
         modeIco.style.color = "#444";
         localStorage.setItem("theme", "light");
     }
@@ -24,7 +30,7 @@ window.onload = function () {
         modeIco.classList.remove("fa-sun");
         modeIco.classList.toggle("fa-moon");
         modeIco.style.color = "#fff";
-        document.body.classList.add("whitetexts");
+        document.body.classList.add("dark-mode");
     }
     preLoader.style.display = "none";
 };
